da79847882c37b2282620641cac112ef9b3ac3cd
Ezhko <ucsd.camp.21@gmail.com>
2021-11-19 15:17:29 -0800

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// $(".surf").click(function() {
+//     window.open("https://ivo-gatzinski.github.io/weather-dash");
+// });
+
+// $(".LED").click(function() {
+//     window.open("https://ivo-gatzinski.github.io/workday-planner");
+// });
+
+// $(".calc").click(function() {
+//     window.open("https://ivo-gatzinski.github.io/password-generator");
+// });
+
+// $(".run").click(function() {
+//     window.open("https://ivo-gatzinski.github.io/adoption-connection");
+// });
+
+// $(".eight_ball").click(function() {
+//     window.open("https://magic8-ezi.herokuapp.com/");
+// });

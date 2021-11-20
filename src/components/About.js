dd904eb9c91726bcef7c114c3af7945d1d895ac8
Ezhko <ucsd.camp.21@gmail.com>
2021-11-20 13:26:17 -0800

    fixed css

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
 <section className="about" id="about">
 <div className="h3">About Me</div>
-<div className="bio card samples">
+<div className="bio">
   <p>I am studying Full Stack Web Development at UCSD Bootcamp.</p>
 </div>
 </section>);

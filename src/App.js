9a94e7860ff1a18c6e0963e8063b20e74b7d85da
Ezhko <ucsd.camp.21@gmail.com>
2021-11-22 21:43:14 -0800

    add condiRender; fix css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header.js";
 import Samples from "./components/Samples.js";
 import ContactInfo from "./components/ContactInfo.js";
@@ -6,12 +6,64 @@ import About from "./components/About.js";
 import Resume from "./components/Resume.js";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("about");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "about":
+        return <About />;
+      case "samples":
+        return <Samples />;
+      // case "contact":
+      //   return </Contact>
+      case "resume":
+        return <Resume />;
+
+      default:
+        return <About />;
+    }
+  };
+
   return (
     <main>
       <Header />
-      <About />
-      <Samples />
-      <Resume />
+      <nav className="subheader nav">
+          <ul>
+            <li>
+              <button
+                className="aboutLink"
+                onClick={() => setCurrentPage("about")}
+              >
+                About
+              </button>
+            </li>
+            <li>
+              <button
+                className="workLink"
+                onClick={() => setCurrentPage("samples")}
+              >
+                Work
+              </button>
+            </li>
+            <li>
+              <button
+                className="contactLink"
+                onClick={() => setCurrentPage("contact")}
+              >
+                Contact
+              </button>
+            </li>
+            <li>
+              <button
+                className="resumeLink"
+                onClick={() => setCurrentPage("resume")}
+              >
+                Resume
+              </button>
+            </li>
+          </ul>
+        </nav>
+      {renderPage()}
       <ContactInfo />
     </main>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,32 +2,13 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div>
-      <section className="space-fill-1"></section>
-      <section className="subheader">
-        <div className="h1">
+      <div><div className="space-fill-1"></div>
+        <div className="subheader h1">
           <h1>Ivo Gatzinski</h1>
         </div>
         <div className="h2">
           <h2>Full Stack Web Developer</h2>
         </div>
-        <nav className="nav">
-          <ul>
-            <li>
-              <button>About</button>
-            </li>
-            <li>
-              <button>Work</button>
-            </li>
-            <li>
-              <button>Contact</button>
-            </li>
-            <li>
-              <button>Resume</button>
-            </li>
-          </ul>
-        </nav>
-      </section>
-    </div>
+      </div>
   );
 }

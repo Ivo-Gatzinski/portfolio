113d297a2566f23886dee0b792e1af5baa325c8b
Ezhko <ucsd.camp.21@gmail.com>
2021-11-23 20:22:02 -0800

    add catch mouse

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,38 +1,68 @@
-import React, {useState} from "react";
-import { validateEmail } from '../utils/helpers';
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { validateEmail } from "../utils/helpers";
 
 export default function Form() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const [email, setEmail] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const handleChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
 
-const handleChange = (e) => {
-const {target} = e;
-const inputType = target.name;
-const inputValue = target.value;
+    if (inputType === "email") {
+      setEmail(inputValue);
+    }
 
+    if (inputType === "name") {
+      setName(inputValue);
+    }
 
-if (inputType === "email") {
-  setEmail(inputValue);
+    if (inputType === "message") {
+      setMessage(inputValue);
+    }
 
-}
+    if (!validateEmail(email)) {
+      setErrorMessage("email is invalid!");
+      return;
+    } else {
+      setErrorMessage("");
+    }
+  };
 
-if (!validateEmail(email)) {
-  setErrorMessage('email is invalid!');
-  return;
-} else {
-  setErrorMessage("");
-}
-
-}; 
+  const handleMouse = () => {
+    setErrorMessage("this field is required");
+    return;
+  };
 
   return (
     <form className="form">
       <div className="inputs">
-        <input type="text" id="input-first" placeholder="Name"></input>
-        <input value={email} type="email" name="email" id="input-email" onChange={handleChange} placeholder="Email"></input>
-        
-        <input type="text" id="input-subject" placeholder="Subject"></input>
+        <input
+          type="text"
+          id="input-name"
+          placeholder="Name"
+          onMouseLeave={handleMouse}
+        ></input>
+        <input
+          value={email}
+          type="email"
+          name="email"
+          id="input-email"
+          onChange={handleChange}
+          onMouseLeave={handleMouse}
+          placeholder="Email"
+        ></input>
+
+        <input
+          type="text"
+          id="input-subject"
+          placeholder="Subject"
+          onMouseLeave={handleMouse}
+        ></input>
       </div>
       <div className="text">
         <textarea
@@ -40,13 +70,9 @@ if (!validateEmail(email)) {
           type="text"
           id="input-message"
           placeholder="Message"
+          onMouseLeave={handleMouse}
         ></textarea>
-        <div>
-    {errorMessage && (
-      
-        <p className="error">{errorMessage}</p>
-      
-    )} </div>
+        <div>{errorMessage && <p className="error">{errorMessage}</p>} </div>
         <button className="submit">Submit</button>
       </div>
     </form>

8b8e98dfd93941ce774e15652200f5589e9a984f
Ezhko <ucsd.camp.21@gmail.com>
2021-11-23 19:38:59 -0800

    add email validate

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
+import { validateEmail } from '../utils/helpers';
 
 export default function Form() {
+
+  const [email, setEmail] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+const handleChange = (e) => {
+const {target} = e;
+const inputType = target.name;
+const inputValue = target.value;
+
+
+if (inputType === "email") {
+  setEmail(inputValue);
+
+}
+
+if (!validateEmail(email)) {
+  setErrorMessage('email is invalid!');
+  return;
+} else {
+  setErrorMessage("");
+}
+
+}; 
+
   return (
     <form className="form">
       <div className="inputs">
-        <input type="text" id="input-first" placeholder="First Name"></input>
-        <input type="text" id="input-last" placeholder="Last Name"></input>
-        <input type="email" id="input-email" placeholder="Email"></input>
+        <input type="text" id="input-first" placeholder="Name"></input>
+        <input value={email} type="email" name="email" id="input-email" onChange={handleChange} placeholder="Email"></input>
+        
         <input type="text" id="input-subject" placeholder="Subject"></input>
       </div>
       <div className="text">
@@ -16,6 +41,12 @@ export default function Form() {
           id="input-message"
           placeholder="Message"
         ></textarea>
+        <div>
+    {errorMessage && (
+      
+        <p className="error">{errorMessage}</p>
+      
+    )} </div>
         <button className="submit">Submit</button>
       </div>
     </form>

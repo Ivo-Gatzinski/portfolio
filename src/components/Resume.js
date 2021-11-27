33ec4aa4484adf66f9eb01442016b7c959634eb6
Ezhko <ucsd.camp.21@gmail.com>
2021-11-26 16:15:42 -0800

    fix resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -45,7 +45,7 @@ export default function Contact() {
           </div>
           <div>
             <ul id="download"><li>
-            <a href="../resume-gatzinski-ivo.pdf" download target="_blank">
+            <a href="../resume-gatzinski-ivo.pdf" target="_blank" rel="noreferrer">
               Download My Resume
             </a></li></ul>
             </div>

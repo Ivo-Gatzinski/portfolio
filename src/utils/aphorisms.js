6070aed3ab9301cd457e882ee4729700e1f76eae
Ezhko <ucsd.camp.21@gmail.com>
2023-01-22 18:37:55 -0800

    add aphorisms scraper

---
+++ src/utils/aphorisms.js
@@ -0,0 +1,21 @@
+var osmosis = require('osmosis');
+var fs = require("fs");
+
+osmosis
+.get('www.dgmlive.com/')
+.find('#app-body > div > div > p')
+.set('aphorism')
+.data(function(listing) {
+    let aphorism = listing.aphorism;
+
+let file = fs.readFileSync('aphorisms.json')
+let data = JSON.parse(file);
+
+data.push(aphorism);
+
+fs.writeFileSync("aphorisms.json", JSON.stringify(data));
+
+})
+.log(console.log)
+.error(console.log)
+.debug(console.log)

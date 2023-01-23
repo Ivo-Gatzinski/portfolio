2d0622233b5cbd085b82bc36220eed5d888282fa
Ezhko <ucsd.camp.21@gmail.com>
2023-01-22 19:13:17 -0800

    get more aphorisms

--- src/utils/aphorisms.js
+++ src/utils/aphorisms.js
@@ -1,21 +1,30 @@
-var osmosis = require('osmosis');
+var osmosis = require("osmosis");
 var fs = require("fs");
 
-osmosis
-.get('www.dgmlive.com/')
-.find('#app-body > div > div > p')
-.set('aphorism')
-.data(function(listing) {
-    let aphorism = listing.aphorism;
 
-let file = fs.readFileSync('aphorisms.json')
-let data = JSON.parse(file);
+function getAphorism() {
+  osmosis
+    .get("www.dgmlive.com/")
+    .find("#app-body > div > div > p")
+    .set("aphorism")
+    .data(function (listing) {
+      let aphorism = listing.aphorism;
 
-data.push(aphorism);
+      let file = fs.readFileSync("aphorisms.json");
+      let data = JSON.parse(file);
 
-fs.writeFileSync("aphorisms.json", JSON.stringify(data));
+      data.push(aphorism);
 
-})
-.log(console.log)
-.error(console.log)
-.debug(console.log)
+      fs.writeFileSync("aphorisms.json", JSON.stringify(data));
+    })
+    .log(console.log)
+    .error(console.log)
+    .debug(console.log);
+}
+
+for (let index = 0; index < 101; index++) {
+  const interval = setInterval(getAphorism, 10000)
+  if (index === 100) {
+    clearInterval(interval);
+  }
+}
